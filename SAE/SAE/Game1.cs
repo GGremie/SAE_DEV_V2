233acfd995cc98fdc1994bcd0aeb559a90730cb5
GGremie <jeremie.serra@etu.univ-smb.fr>
2023-01-04 10:15:26 +0100

    Test

---
+++ SAE/SAE/Game1.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended.Animations;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
+
+namespace SAE
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private TiledMap _tiledMap;
+        private TiledMapRenderer _tiledMapRenderer;
+        private TiledMapTileLayer mapLayer;
+        private int nbZombiesMax = 5;
+
+        /*
+        //Joueur
+        private Vector2 _positionPlayer;
+        private AnimatedSprite _player;
+       //Player playerbase = new Player();
+
+        */
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = false;
+        }
+       
+        protected override void Initialize()
+        {
+            /*
+            //Joueur
+
+            _positionPlayer = new Vector2(0, 0);
+            //
+            */
+
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1920;
+            _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _tiledMap = Content.Load<TiledMap>("map");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+
+
+            // TODO: use this.Content to load your game content here
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            KeyboardState keyboardState = Keyboard.GetState();
+            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds; // DeltaTime
+            float walkSpeed = deltaSeconds; // Vitesse de déplacement du sprite
+
+            /*
+            //player bouge
+            if (keyboardState.IsKeyDown(Keys.Up))
+                _positionPlayer.Y += 1;
+            else if (keyboardState.IsKeyDown(Keys.Down))
+                _positionPlayer.Y -= 1;
+            else if (keyboardState.IsKeyDown(Keys.Left))
+                _positionPlayer.X -= 1;
+            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                _positionPlayer.X += 1;
+            */
+
+            //  Zombie
+            Zombie[] zombieTest = new Zombie[nbZombiesMax];
+            for (int i = 0; i < nbZombiesMax; i++)
+            {
+                zombieTest[i] = new Zombie("Normal");
+            }
+
+                // TODO: Add your update logic here
+                base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _tiledMapRenderer.Draw();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SAE/SAE/Zombie.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+
+namespace SAE
+{
+    internal class Zombie
+    {
+        //Content.RootDirectory = "Content";
+        private Texture2D _textureZombNormal, _textureZombGros, _textureZombRapide;
+        private Vector2 _positionZombie;
+        private int vieZombie, vitesseZombie, nbZombies = 0;
+        private string typeZombie;
+        public const int VIE_NORMAL = 25, VIE_GROS  = 50, VIE_RAPIDE = 10;
+        public const int VITESSE_NORMAL = 25, VITESSE_GROS = 10, VITESSE_RAPIDE = 50;
+        
+
+        public Zombie(string typeZombie)
+        {
+            this.TypeZombie = typeZombie;
+        }
+
+        public string TypeZombie
+        {
+            get
+            {
+                return this.typeZombie;
+            }
+
+            set
+            {
+                this.typeZombie = value;
+            }
+        }
+
+        public Texture2D TextureZombie
+        {
+            get
+            {
+                return this._textureZombNormal;
+            }
+
+            set
+            {
+                this._textureZombNormal = value;
+            }
+        }
+
+        public Vector2 PositionZombie
+        {
+            get
+            {
+                return this._positionZombie;
+            }
+
+            set
+            {
+                this._positionZombie = value;
+            }
+        }
+
+        public void CreationZombie()
+        {
+            nbZombies += 1;
+            switch (this.TypeZombie)
+            {
+                case "Normal":
+                    this.vieZombie = SAE.Zombie.VIE_NORMAL;
+                    this.vitesseZombie = SAE.Zombie.VITESSE_NORMAL;
+                    break;
+                case "Rapide":
+                    this.vieZombie = SAE.Zombie.VIE_RAPIDE;
+                    this.vitesseZombie = SAE.Zombie.VITESSE_RAPIDE;
+                    break;
+                case "Gros":
+                    this.vieZombie = SAE.Zombie.VIE_GROS;
+                    this.vitesseZombie = SAE.Zombie.VITESSE_GROS;
+                    break;
+
+            }
+        }
+        public int NbZombies()
+        {
+            return nbZombies;
+
+        }
+
+    }
+}
